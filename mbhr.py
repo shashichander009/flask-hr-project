9650b49ce6049a345688a8331031af9a16f60862
Shashi Kumar Sah <shashichander009@gmail.com>
2020-08-24 09:49:54 +0530

    font changes

--- mbhr.py
+++ mbhr.py
@@ -1,8 +1,2 @@
 
-from app import app, db
-from app.models import Admin, Employee
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Admin': Admin, 'Employee': Employee}
+from app import app
